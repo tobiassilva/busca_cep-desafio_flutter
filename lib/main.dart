e68dbaecc3be4f42d372bdfc1b4b7bf0090b00d6
Tobias Arantes <tobiasarantes0@gmail.com>
2022-04-06 18:58:49 -0300

    nome app

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Buscador de CEP',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
